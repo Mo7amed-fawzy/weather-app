2f94e7634b1c5cf0c345d72f44a47b858cb632d5
Mo7amed-fawzy <m07amed1st@gmail.com>
2024-10-17 23:08:06 +0200

    create assets and constans files

---
+++ lib/constants/constants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/foundation.dart' show immutable;
+
+// بجيب عنصر واحد بس من المكتبة الهو immutable
+@immutable
+class Constants {
+  static const String apiKey = '7ac000ad65981ddaf389a014e439b0b3';
+  static const String apiKey2 = '19d2fd7aab958bfe7f61000e1f5151cc';
+}
